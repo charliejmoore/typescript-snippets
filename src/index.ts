711460ba12f1f4acece7d9fb6296eb7a7c8acb65
Charlie Moore <moorejchar@gmail.com>
2021-09-11 17:01:07 -0400

    chore: 🔨 add dependencies for docs

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * as ArrayUtils from "../src/array-utils";
+export * as DateUtils from "../src/date-utils";
+export * as ObjectUtils from "../src/object-utils";
+export * as FileUtils from "../src/file-utils";
+export * as RandomUtils from "../src/random-utils";
