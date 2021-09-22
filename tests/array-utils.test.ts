5a0eca97ad7f5a2c3d24e853f251c18422d503d5
Charlie Moore <moorejchar@gmail.com>
2021-09-21 20:16:44 -0400

    refactor: 🚚 move array-utils tests to the right test file

--- tests/array-utils.test.ts
+++ tests/array-utils.test.ts
@@ -1,12 +1,65 @@
-import { flatten, getRandomElement, removeFromArray } from "../src/array-utils";
+import { flatten, removeFromArray } from '../src/array-utils';
 
-describe('test #flatten function', () => {
-
-  it('should return the input list if the input list was already flat', () => {
-    expect(flatten([1, 2, 3])).toEqual([1, 2, 3]);
+describe('test #removeFromArray function', () => {
+  it('should not alter an array when the element is not part of the array', () => {
+    const array = [1, 2, 3];
+    expect(removeFromArray(4, array)).toEqual([1, 2, 3]);
   });
 
-  it('should return a flattened list if the input list was not flat', () => {
-    expect(flatten([1, [2, 3, [[4]]]])).toEqual([1, 2, 3, 4]);
+  it('should not alter an array when the element is not part of the array', () => {
+    const array: any[] = [];
+    expect(removeFromArray(4, array)).toEqual([]);
+  });
+
+  it('should remove an element from an array if it is present', () => {
+    const array = [1, 2, 3];
+    expect(removeFromArray(2, array)).toEqual([1, 3]);
+  });
+
+  it('should remove an element from an array if it is present', () => {
+    const array = [1];
+    expect(removeFromArray(1, array)).toEqual([]);
+  });
+
+  it('should remove an all occurances element from an array if it is present multiple times', () => {
+    const array = [1, 2, 3, 3, 3, 3];
+    expect(removeFromArray(3, array)).toEqual([1, 2]);
+  });
+
+  it('should remove a string from an array if it is present', () => {
+    const array = ['abc', 'def', 'ghi'];
+    expect(removeFromArray('def', array)).toEqual(['abc', 'ghi']);
+  });
+});
+
+describe('test #flatten function', () => {
+  it('should return the the inputted array when the array is already flat', () => {
+    const array = [1, 2, 3];
+    expect(flatten(array)).toEqual([1, 2, 3]);
+  });
+
+  it('should return the the inputted array when the array is already flat', () => {
+    const array = [1];
+    expect(flatten(array)).toEqual([1]);
+  });
+
+  it('should return the the inputted array when the array is already flat', () => {
+    const array: any[] = [];
+    expect(flatten(array)).toEqual([]);
+  });
+
+  it('should return the the inputted array when the array is not flat', () => {
+    const array = [[1]];
+    expect(flatten(array)).toEqual([1]);
+  });
+
+  it('should return the the inputted array when the array is not flat', () => {
+    const array = [[], [], [[[]]]];
+    expect(flatten(array)).toEqual([]);
+  });
+
+  it('should return the the inputted array when the array is not flat', () => {
+    const array = [[2, [3]], [4], [[3, [7]]]];
+    expect(flatten(array)).toEqual([2, 3, 4, 3, 7]);
   });
 });
