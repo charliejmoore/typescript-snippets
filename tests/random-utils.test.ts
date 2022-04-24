059c36210839ed424329c4f474ea2cf7c99b75b0
Charlie Moore <moorejchar@gmail.com>
2022-04-24 16:00:24 -0400

    test: ✅ Add test

--- tests/random-utils.test.ts
+++ tests/random-utils.test.ts
@@ -3,8 +3,11 @@ import { RandomUtils } from '../src/random-utils';
 describe('RandomUtils', () => {
   describe('test #getRandomInRange function', () => {
     it('should return a number that is less than or equal to `max`', () => {
+      const min: number = 0;
       const max: number = 10;
-      expect(RandomUtils.getRandomInRange(max)).toBeLessThanOrEqual(max);
+      const result: number = RandomUtils.getRandomInRange(min, max);
+      expect(result).toBeLessThanOrEqual(max);
+      expect(result).toBeGreaterThan(min);
     });
   });
 });
