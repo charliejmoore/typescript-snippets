c4b48ed0f968f487e29f68693e7686de9fc6b0a6
Charlie Moore <moorejchar@gmail.com>
2022-04-24 16:02:18 -0400

    feat: ✨ Add randomBoolean function

--- src/random-utils.ts
+++ src/random-utils.ts
@@ -2,5 +2,8 @@ export class RandomUtils {
   static getRandomInRange(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+
+  static getRandomBoolean(): boolean {
+    return Math.random() >= 0.5;
   }
 }
