71f2ecac11076cb06593f17d917401fa35980898
Charlie Moore <moorejchar@gmail.com>
2022-04-24 16:01:43 -0400

    feat: ✨ Add more parameters to random range function

--- src/random-utils.ts
+++ src/random-utils.ts
@@ -1,5 +1,6 @@
 export class RandomUtils {
-  static getRandomInRange(max: number): number {
-    return Math.floor(Math.random() * Math.floor(max));
+  static getRandomInRange(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
   }
 }
